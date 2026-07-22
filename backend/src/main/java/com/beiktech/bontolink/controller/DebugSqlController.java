ebc97e2004c66b6a76c25535cef1a9c11cce75e9
wangbj <wangbj@beiktech.com>
2026-07-22 10:01:22 +0800

    修复 DebugSqlController 导入路径错误 (model.R -> common.R)

--- backend/src/main/java/com/beiktech/bontolink/controller/DebugSqlController.java
+++ backend/src/main/java/com/beiktech/bontolink/controller/DebugSqlController.java
@@ -1,6 +1,6 @@
 package com.beiktech.bontolink.controller;
 
-import com.beiktech.bontolink.model.R;
+import com.beiktech.bontolink.common.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
